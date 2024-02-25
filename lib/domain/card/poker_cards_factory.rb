965b3a58aa9d2578bc7b60e4a1bf7e3a2d3cf57f
lukblan <luk.blanko@gmail.com>
2024-02-25 09:31:33 -0300

    Rename cards factory name

--- lib/domain/card/poker_cards_factory.rb
+++ lib/domain/card/poker_cards_factory.rb
@@ -1,6 +1,6 @@
 require_relative 'card'
 
-class CardsFactory
+class PokerCardsFactory
   def create_cards
     cards = []
     cards_values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
